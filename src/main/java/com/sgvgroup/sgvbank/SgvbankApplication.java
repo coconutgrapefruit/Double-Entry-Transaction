2942cf99f696de3d432abfa0b340a2810286f996
coconutgrapefruit <massmooses@gmail.com>
2025-05-17 18:34:32 +0900

    init

---
+++ src/main/java/com/sgvgroup/sgvbank/SgvbankApplication.java
@@ -0,0 +1,13 @@
+package com.sgvgroup.sgvbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgvbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgvbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sgvgroup/sgvbank/SgvbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgvgroup.sgvbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgvbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
