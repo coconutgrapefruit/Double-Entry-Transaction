5d84b80193c52907b089676375a3b71cc5cab5c2
coconutgrapefruit <massmooses@gmail.com>
2025-05-26 23:55:40 +0900

    add unique constrain and idx

--- src/db-init/01_schema.sql
+++ src/db-init/01_schema.sql
@@ -4,6 +4,7 @@ create table if not exists accounts (
     balance numeric(19,4) not null default 0 check (balance >= 0),
     version bigint NOT NULL DEFAULT 0
 );
+
 create table if not exists ledger_entries (
     id UUID primary key default gen_random_uuid(),
     account_id UUID not null references accounts(id),
@@ -11,7 +12,9 @@ create table if not exists ledger_entries (
     amount numeric(19,4) not null,
     entry_type varchar(6) not null check(entry_type in (DEBIT, CREDIT)),
     created_at timestampz not null default now()
+    unique (transaction_id, entry_type)
 );
+
 create table if not exists transactions (
     id UUID primary key default gen_random_uuid(),
     from_account_id UUID not null references accounts(id),
@@ -19,6 +22,10 @@ create table if not exists transactions (
     amount numeric(19,4) not null,
     created_at timestampz not null default now()
 )
+
+create index idx_tx_from_created on transactions(from_account_id, created_at desc);
+create index idx_tx_to_created on transactions(to_account_id, created_at desc);
+
 create table if not exists outbox (
     id bigint primary key auto_increment,
     aggregate_type varchar(20),
